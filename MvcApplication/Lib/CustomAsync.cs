6e0bc60162451ac0c9028ab58b21f6684349a9c6
Boychenko <b0sanya@gmail.com>
2013-07-08 16:36:36 +0700

    custom awaitable

---
+++ MvcApplication/Lib/CustomAsync.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication.Lib
+{
+    public class CustomAsync
+    {
+        public CustomAwaitable DoWork()
+        {
+            return new CustomAwaitable();
+        }
+    }
+}
---
+++ MvcApplication/Lib/CustomAwaitable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Web;
+
+namespace MvcApplication.Lib
+{
+    public class CustomAwaitable : INotifyCompletion
+    {
+        public CustomAwaitable GetAwaiter()
+        {
+            return this;
+        }
+        public bool IsCompleted
+        {
+            get
+            {
+                return true;
+            }
+        }
+        public void OnCompleted(Action continuation)
+        {
+            var ctx = SynchronizationContext.Current;
+            var t = new Timer(
+                delegate
+                {
+                    ctx.Post(state => continuation(), null);
+                }, null, 5000, -1);
+        }
+        public string GetResult()
+        {
+            return "foo";
+        }
+    }
+}
