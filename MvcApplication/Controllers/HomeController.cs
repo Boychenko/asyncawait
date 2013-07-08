fb3d26333641a5a36e2bd4b348e55d3d0c5e515b
Boychenko <b0sanya@gmail.com>
2013-07-08 16:39:18 +0700

    reworked to async

--- MvcApplication/Controllers/HomeController.cs
+++ MvcApplication/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -15,13 +16,15 @@ namespace MvcApplication.Controllers
         //
         // GET: /Home/
         [UseStopwatch]
-        public ActionResult Index()
+        [AsyncTimeout(2000)]
+        public async Task<ActionResult> Index(CancellationToken cancellationToken)
         {
             var request = HttpContext.Request;
             var baseUrl = String.Format("{0}://{1}/api/", request.Url.Scheme, request.Url.Authority);
-            var foo = new WebClient().DownloadString(baseUrl + "foo");
-            var bar = new WebClient().DownloadString(baseUrl + "bar");
-            ViewBag.Text = foo + bar;
+            var foo = new WebClient().DownloadStringTaskAsync(baseUrl + "foo");
+            var bar = new WebClient().DownloadStringTaskAsync(baseUrl + "bar");
+            var results = await Task.WhenAll(foo, bar);
+            ViewBag.Text = String.Join("", results);
             return View();
         }
     }
